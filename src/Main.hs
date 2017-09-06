4f1289fb0d3f1941af4c2ef27db2364a00342283
Maxim Samoilov <nitive@icloud.com>
2017-09-06 23:14:44 +0300

    Add basic web page

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+import Web.Scotty (html, param, get, scotty)
+import Data.Monoid (mconcat)
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+template = mconcat
+  [ "<!DOCTYPE html>"
+  , "<html lang='en'>"
+  ,   "<head>"
+  ,     "<meta charset='UTF-8'>"
+  ,     "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+  ,     "<meta http-equiv='X-UA-Compatible' content='ie=edge'>"
+  ,     "<title>App</title>"
+  ,   "</head>"
+  ,   "<body>"
+  ,     "<div id='app'>Loading...</div>"
+  ,   "</body>"
+  , "</html>"
+  ]
+
+main = scotty 3000 $ do
+  get "/" $ do
+    html template
