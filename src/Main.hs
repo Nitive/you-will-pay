03a53050ea6a09c36547cbd716f75bdf38752bc6
Maxim Samoilov <nitive@icloud.com>
2017-09-07 00:10:01 +0300

    Use blaze-html to render page template

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Web.Scotty (html, param, get, scotty)
-import Data.Monoid (mconcat)
 
-template = mconcat
-  [ "<!DOCTYPE html>"
-  , "<html lang='en'>"
-  ,   "<head>"
-  ,     "<meta charset='UTF-8'>"
-  ,     "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-  ,     "<meta http-equiv='X-UA-Compatible' content='ie=edge'>"
-  ,     "<title>App</title>"
-  ,   "</head>"
-  ,   "<body>"
-  ,     "<div id='app'>Loading...</div>"
-  ,   "</body>"
-  , "</html>"
-  ]
+import Web.Scotty as S
+import Text.Blaze.Html5 as H hiding (main)
+import Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+
+template = do
+  docTypeHtml $ do
+    H.html ! lang "en" $ do
+      H.head $ do
+        H.meta ! charset "utf-8"
+        H.meta ! name "viewport" ! content "width=device-width, initial-scale=1.0"
+        H.meta ! httpEquiv "X-UA-Compatible" ! content "ie=edge"
+        H.title "App"
+      H.body $ do
+        H.div "Loading..."
 
 main = scotty 3000 $ do
   get "/" $ do
-    html template
+    S.html . renderHtml $ template
