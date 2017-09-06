681ca7abbaf9993178c6657fea44d0a0d48936e4
Maxim Samoilov <nitive@icloud.com>
2017-09-07 00:14:52 +0300

    Use qualified imports

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Web.Scotty as S
-import Text.Blaze.Html5 as H hiding (main)
-import Text.Blaze.Html5.Attributes as A
+import qualified Web.Scotty as S
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5 ((!))
+import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 template = do
-  docTypeHtml $ do
-    H.html ! lang "en" $ do
+  H.docTypeHtml $ do
+    H.html ! A.lang "en" $ do
       H.head $ do
-        H.meta ! charset "utf-8"
-        H.meta ! name "viewport" ! content "width=device-width, initial-scale=1.0"
-        H.meta ! httpEquiv "X-UA-Compatible" ! content "ie=edge"
+        H.meta ! A.charset "utf-8"
+        H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1.0"
+        H.meta ! A.httpEquiv "X-UA-Compatible" ! A.content "ie=edge"
         H.title "App"
       H.body $ do
         H.div "Loading..."
 
-main = scotty 3000 $ do
-  get "/" $ do
+main = S.scotty 3000 $ do
+  S.get "/" $ do
     S.html . renderHtml $ template
