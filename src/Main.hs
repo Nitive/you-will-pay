3413be6d8657b0f08cfc42d396a4ad3c82eaa5d4
Maxim Samoilov <nitive@icloud.com>
2017-09-14 10:27:02 +0300

    Add mock api

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified Web.Scotty as S
@@ -6,6 +7,8 @@ import qualified Text.Blaze.Html5 as H
 import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Text (renderHtml)
+import Data.Aeson (Value (Null), (.=), object)
+import Server.Api.Api (SummaryReport(..), User(..), Transaction(..))
 
 template = do
   H.docTypeHtml $ do
@@ -16,9 +19,23 @@ template = do
         H.meta ! A.httpEquiv "X-UA-Compatible" ! A.content "ie=edge"
         H.title "App"
       H.body $ do
-        H.div "Loading..."
         H.script ! A.src "http://localhost:1337/app.js" $ ""
 
 main = S.scotty 3000 $ do
   S.get "/" $ do
     S.html . renderHtml $ template
+
+  S.get "/api/summary/" $ do
+    S.json SummaryReport
+      { payUserName = "Jon"
+      , payDiff = 10
+      , users = [User { name = "Jon", color = "#50e3c2" }]
+      , history =
+        [ Transaction
+          { time = "Thu Sep 14 2017 10:19:46 GMT+0300 (MSK)"
+          , userName = "Jon"
+          , price = 100
+          , description = "Дал Арье на карманные расходы"
+          }
+        ]
+      }
---
+++ src/Server/Api/Api.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Server.Api.Api
+( SummaryReport(..)
+, User(..)
+, Transaction(..)
+) where
+
+import Data.Aeson
+import GHC.Generics
+
+data User = User
+  { name :: String
+  , color :: String
+  } deriving (Generic, Show)
+
+instance FromJSON User
+instance ToJSON User
+
+data Transaction = Transaction
+  { time :: String
+  , userName :: String
+  , price :: Int
+  , description :: String
+  } deriving (Generic, Show)
+
+instance FromJSON Transaction
+instance ToJSON Transaction
+
+data SummaryReport = SummaryReport
+  { payUserName :: String
+  , payDiff :: Int
+  , users :: [User]
+  , history :: [Transaction]
+  } deriving (Generic, Show)
+
+instance FromJSON SummaryReport
+instance ToJSON SummaryReport
