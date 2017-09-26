3dac015f22eae011f4a5964732481925fcd60238
Maxim Samoilov <nitive@icloud.com>
2017-09-26 15:45:43 +0300

    Create transaction with current time

--- src/Frontend/Main.purs
+++ src/Frontend/Main.purs
@@ -1,13 +1,22 @@
 module Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Now (NOW)
 import Halogen.Aff (HalogenEffects, awaitBody, runHalogenAff)
 import Halogen.VDom.Driver (runUI)
 import Network.HTTP.Affjax (AJAX)
 import Screens.Room (room)
 
-main :: Eff (HalogenEffects (ajax :: AJAX)) Unit
+type AppEffects = HalogenEffects
+  ( ajax :: AJAX
+  , console :: CONSOLE
+  , now :: NOW
+  )
+
+main :: Eff AppEffects Unit
 main = runHalogenAff do
   body <- awaitBody
   runUI room unit body
--- src/Frontend/Screens/Room.purs
+++ src/Frontend/Screens/Room.purs
@@ -4,8 +4,13 @@ import Prelude
 
 import Api.AddTransaction (AddTransactionRequest(..), AddTransactionResponse(..), SuccessResponse(..), addTransaction)
 import Control.Monad.Aff (Aff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Now (NOW, now)
 import DOM (DOM)
 import DOM.Event.Event (Event, preventDefault)
+import Data.DateTime.Instant (toDateTime)
+import Data.Either (either)
+import Data.Formatter.DateTime (formatDateTime)
 import Data.Maybe (Maybe(..))
 import Halogen (liftEff)
 import Halogen as H
@@ -26,7 +31,7 @@ type State =
 data Query a
   = SubmitForm Event a
 
-room :: forall eff. H.Component HH.HTML Query Unit Void (Aff (ajax :: AJAX, dom :: DOM | eff))
+room :: forall eff. H.Component HH.HTML Query Unit Void (Aff (ajax :: AJAX, dom :: DOM, console :: CONSOLE, now :: NOW | eff))
 room =
   H.component
     { initialState: const initialState
@@ -50,22 +55,23 @@ room =
             , HH.button [] [ HH.text "test" ]
             ]
 
-    eval :: Query ~> H.ComponentDSL State Query Void (Aff (ajax :: AJAX, dom :: DOM | eff))
+    eval :: Query ~> H.ComponentDSL State Query Void (Aff (ajax :: AJAX, dom :: DOM, console :: CONSOLE, now :: NOW | eff))
     eval = case _ of
       SubmitForm event next -> do
+        created <- liftEff $ toDateTime <$> now
         liftEff $ preventDefault event
 
         H.modify (_ { loading = true })
         let request = AddTransactionRequest {
-              created: "2017-09-25 10:00:00+03"
+              created: either id id $ formatDateTime "YYYY-MM-DD hh:mm:ss+03" created
             , userId: 1
             , price: 123
             , description: "some words"
             , roomId: 1
             }
 
-        req <- H.liftAff $ addTransaction request
-        let (SuccessResponse response) = (req.response :: SuccessResponse AddTransactionResponse)
+        res <- H.liftAff $ addTransaction request
+        let (SuccessResponse response) = (res.response :: SuccessResponse AddTransactionResponse)
         let (AddTransactionResponse result) = response.result
         let summary = { transactionId: result.transactionId } :: Summary
         H.modify (_ { loading = false, summary = Just summary })
