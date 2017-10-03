014309dfbc1f33410a5770278aff991abfa4f256
Maxim Samoilov <nitive@icloud.com>
2017-10-04 02:44:22 +0300

    fix init db scripts: create user before db

--- init_db/init_local_db.sh
+++ init_db/init_local_db.sh
@@ -1,11 +1,11 @@
-# create db (remove if exist)
-dropdb ywp_db || true
-createdb ywp_db
-
 # create user (remove if exist)
 dropuser ywp_user || true
 createuser ywp_user
 
+# create db (remove if exist)
+dropdb ywp_db --username=ywp_user || true
+createdb ywp_db --username=ywp_user
+
 # create tables and insert data
-psql -f init_local_db/create_shape.sql ywp_db
-psql -f init_local_db/create_data.sql ywp_db
+psql -f init_db/create_shape.sql ywp_db
+psql -f init_db/create_data.sql ywp_db
--- init_db/init_prod_db.sh
+++ init_db/init_prod_db.sh
@@ -1,8 +1,8 @@
-# create db (skip if exist)
-createdb ywp_db || true
-
 # create user (skip if exist)
 createuser ywp_user || true
 
+# create db (skip if exist)
+createdb ywp_db --username=ywp_user || true
+
 # create tables
-psql -f init_local_db/create_shape.sql ywp_db
+psql -f init_db/create_shape.sql ywp_db
