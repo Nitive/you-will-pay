8c7620104952a204937e8f405eb9ac3533538206
Maxim Samoilov <nitive@icloud.com>
2017-10-18 01:19:41 +0300

    feat(transactionsList): add circles

--- src/Frontend/Components/TransactionsList.purs
+++ src/Frontend/Components/TransactionsList.purs
@@ -1,14 +1,15 @@
 module Components.TransactionsList where
 
-import CSS (StyleM, color, fontSize, marginBottom, paddingLeft, px)
+import CSS (Color, StyleM, backgroundColor, borderRadius, color, display, fontSize, fromHexString, height, inlineBlock, marginBottom, marginRight, paddingLeft, pct, px, width)
 import CSS.Common (none)
 import CSS.ListStyle.Type (listStyleType)
 import Data.Int (toNumber)
-import Halogen.HTML (HTML, li, text, ul)
+import Data.Maybe (fromMaybe)
+import Halogen.HTML (HTML, li, span, text, ul)
 import Halogen.HTML.CSS (style)
 import Prelude (Unit, discard, negate, ($), (<$>), (<>))
 import Screens.Room.Model (Transaction)
-import UI.Colors (warmGrey)
+import UI.Colors (warmGrey, white)
 import Utils.Formatters (formatCurrency)
 
 listStyle :: StyleM Unit
@@ -23,9 +24,22 @@ itemStyle :: StyleM Unit
 itemStyle = do
   marginBottom $ px 10.0
 
+circleStyle :: Color -> StyleM Unit
+circleStyle userColor = do
+  display inlineBlock
+  height $ px 10.0
+  width $ px 10.0
+  marginRight $ px 5.0
+  borderRadius (pct 100.0) (pct 100.0) (pct 100.0) (pct 100.0)
+  backgroundColor userColor
+
 transactionsList :: forall p i. Array Transaction -> HTML p i
 transactionsList transactions = ul [ style listStyle ] $ renderTransaction <$> transactions
   where
-    renderTransaction tr = li [ style itemStyle ] [ text description ]
+    renderTransaction tr = li [ style itemStyle ]
+      [ span [ style $ circleStyle circleColor ] []
+      , text description
+      ]
       where
+        circleColor = fromMaybe white $ fromHexString tr.user.color
         description = tr.user.name <> ", " <> (formatCurrency $ toNumber tr.price) <> " ₽, «" <> tr.description <> "»"
