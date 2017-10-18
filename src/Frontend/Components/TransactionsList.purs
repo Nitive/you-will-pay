83f43ecbc97df5d6aad7d3937b4af2d5cfe33ace
Maxim Samoilov <nitive@icloud.com>
2017-10-18 10:11:49 +0300

    Add line before history list

--- src/Frontend/Components/TransactionsList.purs
+++ src/Frontend/Components/TransactionsList.purs
@@ -1,12 +1,12 @@
 module Components.TransactionsList where
 
-import CSS (Color, StyleM, absolute, backgroundColor, borderRadius, bottom, color, display, fontSize, fromHexString, height, inlineBlock, left, marginBottom, marginRight, paddingBottom, paddingLeft, pct, position, px, relative, top, width)
+import CSS (Color, StyleM, absolute, backgroundColor, borderRadius, bottom, color, display, fontSize, fromHexString, height, inlineBlock, left, marginBottom, marginRight, marginTop, paddingBottom, paddingLeft, pct, position, px, relative, top, width)
 import CSS.Common (none)
 import CSS.ListStyle.Type (listStyleType)
 import Data.Array (length, mapWithIndex)
 import Data.Int (toNumber)
 import Data.Maybe (fromMaybe)
-import Halogen.HTML (HTML, div, li, text, ul)
+import Halogen.HTML (HTML, div, div_, li, text, ul)
 import Halogen.HTML.CSS (style)
 import Prelude (Unit, discard, negate, ($), (-), (/=), (<>))
 import Screens.Room.Model (Transaction)
@@ -15,7 +15,8 @@ import Utils.Formatters (formatCurrency)
 
 listStyle :: StyleM Unit
 listStyle = do
-  marginBottom $ px (-10.0)
+  marginTop $ px 15.0
+  marginBottom $ px $ -10.0
   paddingLeft $ px 16.0
   listStyleType none
   fontSize $ px 13.0
@@ -41,22 +42,35 @@ circleStyle userColor = do
 lineStyle :: StyleM Unit
 lineStyle = do
   position absolute
-  top $ px 4.0
-  left $ px $ -11.0
-  bottom $ px $ -4.0
   width $ px 1.0
   backgroundColor warmGray
 
+lineBetweenStyle :: StyleM Unit
+lineBetweenStyle = do
+  lineStyle
+  top $ px 4.0
+  left $ px $ -11.0
+  bottom $ px $ -4.0
+
+lineBeforeStyle :: StyleM Unit
+lineBeforeStyle = do
+  lineStyle
+  top $ px $ -11.0
+  left $ px $ 5.0
+  height $ px 15.0
+
 transactionsList :: forall p i. Array Transaction -> HTML p i
 transactionsList [] = text "No transactions yet"
-transactionsList transactions =
-    ul [ style listStyle ] $ mapWithIndex renderTransaction transactions
+transactionsList transactions = div [ style $ position relative ]
+  [ div [ style lineBeforeStyle ] []
+  , ul [ style listStyle ] $ mapWithIndex renderTransaction transactions
+  ]
 
   where
     renderTransaction index tr = li [ style itemStyle ]
       (line <> [ div [ style $ circleStyle circleColor ] [], text description ])
       where
         withLine = index /= length transactions - 1
-        line = if withLine then [ div [ style lineStyle ] [] ] else []
+        line = if withLine then [ div [ style lineBetweenStyle ] [] ] else []
         circleColor = fromMaybe white $ fromHexString tr.user.color
         description = tr.user.name <> ", " <> (formatCurrency $ toNumber tr.price) <> " ₽, «" <> tr.description <> "»"
